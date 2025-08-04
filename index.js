08058da4edd3ecf08b3e8391e4fabdfe536643c8
Natnael Mesfin <natnael.tesfaye2022@gmail.com>
2025-08-04 09:35:27 +0200

    bug fixes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import env from "dotenv";
+
+env.config();
 
 const app = express();
 const port = 3000;
@@ -9,7 +12,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "123456",
+  password: process.env.PASSWORD,
   port: 5432,
 });
 db.connect();
@@ -19,10 +22,7 @@ app.use(express.static("public"));
 
 let currentUserId = 1;
 
-let users = [
-  { id: 1, name: "Angela", color: "teal" },
-  { id: 2, name: "Jack", color: "powderblue" },
-];
+let users = [];
 
 async function checkVisisted() {
   const result = await db.query(
